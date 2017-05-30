ecebca042867f91c4baf179ed675c5a785d7f920
rkeshmir@yahoo.com <autumn2014>
2017-05-31 03:38:39 +0430

    dynamic services, animated logo

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,16 +6,29 @@
 		.controller('MainController', MainController);
 	
 	/** @ngInject */
-	function MainController($document, $translate, toastr) {
+	function MainController($document, $translate, $timeout) {
 		var firstLogin = $document[0].cookie.replace(
 			/(?:(?:^|.*;\s*)firstLogin\s*\=\s*([^;]*).*$)|^.*$/, '$1'
 		);
 		var vm = this;
 		vm.mouseIn = mouseIn;
 		vm.mouseOut = mouseOut;
-		vm.icon = {
-			"star": 1
-		};
+
+        vm.services = [
+			{
+				name: "برندسازی",
+				logoOut: "../../assets/images/icon/star-Start.jpg",
+				logoAnimateIn: "../../assets/images/icon/star.gif",
+				logoIn: "../../assets/images/icon/star-End.jpg",
+				logoAnimateOut: "../../assets/images/icon/star-revers.gif",
+				id: 1
+			}
+		];
+
+        vm.services.forEach(function (service) {
+            service.icon = 0;
+		});
+
 		activate();
 		
 		function activate() {
@@ -30,14 +43,36 @@
 			
 		}
 
-		function mouseIn() {
-			console.log('mouse in');
-			vm.icon['star'] = 2;
+
+		var iconTimeOut = 900;
+
+		function mouseIn(service) {
+			console.log('mouse in', service.id);
+			var src = service.logoAnimateIn;
+            var $img = $('#icon-' + service.id + '-1');
+            var timeout = 0; // no delay
+            $timeout(function() {
+                $img.attr('src', src);
+            }, timeout);
+            service.icon = 1;
+            $timeout(function () {
+                service.icon = 2;
+            }, iconTimeOut);
+
 		}
 
-		function mouseOut() {
-			console.log('mouse out');
-			vm.icon['star'] = 1;
+		function mouseOut(service) {
+			console.log('mouse out', service.id);
+            var src = service.logoAnimateOut;
+            var $img = $('#icon-' + service.id + '-2');
+            var timeout = 0; // no delay
+            $timeout(function() {
+                $img.attr('src', src);
+            }, timeout);
+            service.icon = 3;
+            $timeout(function () {
+                service.icon = 0;
+            }, iconTimeOut);
 		}
 	}
 })();
