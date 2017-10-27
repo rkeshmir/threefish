9b159fd7e51ed9d9aa7f3ea437af573d7ac14467
rkeshmir@yahoo.com <autumn2014>
2017-10-28 00:01:28 +0330

    new icons, services in responsive design

--- semahi/client/public/gulp/server.js
+++ semahi/client/public/gulp/server.js
@@ -12,7 +12,7 @@ var util = require('util');
 var proxyMiddleware = require('http-proxy-middleware');
 
 function browserSyncInit(baseDir, browser) {
-    browser = browser === undefined ? 'firefox' : browser;
+    browser = browser === undefined ? 'default' : browser;
 	
 	var routes = null;
 	if (baseDir === conf.paths.src || (util.isArray(baseDir) && baseDir.indexOf(conf.paths.src) !== -1)) {
